ee1bf6a0b947b9e787e04b8c2de34ef8845f8edd
keee99 <kohjj99@gmail.com>
2023-10-01 17:15:49 +0800

    Lab-3: Increase score from coin

---
+++ Assets/Scripts/Week3/AnimationEventIntTool.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimationEventIntTool : MonoBehaviour
+{
+    public int parameter;
+    public UnityEvent<int> useInt;
+
+    public void TriggerIntEvent()
+    {
+        useInt.Invoke(parameter);
+    }
+}
